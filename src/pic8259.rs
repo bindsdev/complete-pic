b6b0ff98974741027f47ca83fdff788d32effff2
bindsdev <bindsdev@gmail.com>
2023-04-07 12:10:49 -0500

    initial 8259 PIC interface complete

--- src/pic8259.rs
+++ src/pic8259.rs
@@ -1,35 +1,26 @@
 //! 8269 PIC interface
 //!
 //! # What is the 8259 PIC?
-//! The 8259 PIC (or Programmable Interrupt Controller) is a crucial component of the x86 architecture.
-//! It led to the x86 architecture becoming interrupt-driven. Its purpose is to manage hardware interrupts
-//! by sending them to the appropriate system interrupt. This allows the system to repond to the needs of devices
-//! without losing time. In modern systems, the 8259 PIC has been replaced by the APIC (Advanced Programmable Interrupt Controller),
-//! which is usable with multiple cores/processors.
-//!
-//! ## What exactly does the 8259 PIC do?
-//! The 8259 PIC controls the interrupt mechanism of the CPU. It does this by feeding multiple interrupt requests, in order, to the
-//! processor. A hardware interrupt will send a pulse along its interrupt line to the 8259 PIC. The 8259 PIC will then
-//! translate the IRQ (Interrupt Request)/Hardware Interrupt into a system interrupt. It will then send a message to the CPU, interrupting
-//! whatever task it was doing. The OS kernel should handle these IRQs and perform the necessary procedure (like polling the keyboard for a scancode)
-//! or alert a userspace program of an interrupt by sending a message to a driver.
-//!
-//! ## What would be different if the 8259 PIC didn't exist?
-//! Without the 8259 PIC, you would have to manually poll devices in the system to figure out if they want to do anything. You
-//! would then waste time trying to go to these devices to figure out what they want to do. The 8259 PIC makes it easy by allowing the devices
-//! to present themselves to you when they are ready to carry out an event.
+//! The 8259 PIC (or Programmable Interrupt Controller) is a crucial device in the x86 architecture.
+//! Its purpose is to manage hardware interrupts by sending them to the appropriate system interrupt, allowing the system to respond to hardware devices
+//! without losing time. The 8259 PIC makes handling hardware interrupts much easier. Without it, the hardware devices would have to be manually polled
+//! to check if they have anything they want to do. Time would be wasted trying to go to the hardware device and figure out what it wants to do.
 //!
 //! ## How does the 8259 PIC work?
-//! Modern systems contain 2 8259 PICs, each with 8 inputs. One is called the "master" and the other is called the "slave". If any input on the PIC is raised,
-//! it sets a bit interanlly that signals that the input needs servicing. Next, it checks if that channel is masked or not, and whether an interrupt is already pending.
-//! If the channel is unmasked and no interrupt is pending, the PIC raises the interrupt line. The slave then feeds the IRQ number to the master and the master connects to the interrupt line.
-//! After the processor accepts the interrupt, the master checks which of the PICs is reponsible for answering. It then eithr feeds the interrupt number to the processor or asks the slave to.
-//! The PIC that answers, whether it be the master or slave, looks for the "vector offet" and adds it to the input line to compute the interrupt number. The processor
-//! then acts on that interrupt address.
+//! In a modern system, there are 2 8259 PICs, each with 8 inputs. The first is called the "master" while the other is called the "slave". If any input on the PIC is raised,
+//! it will signal that the input needs servicing by setting an internal bit. The PIC then does two checks. It checks whether that input is masked or not, and whether another interrupt
+//! is already pending. If the input is unmasked and no interrupt is pending, the PIC will raise the interrupt line. The slave PIC will send the IRQ number to the master so it can connect to
+//! the interrupt line. The master PIC will then check which PIC is responsible for answering the interrupts. If the master PIC is responsible, it will send the interrupt number to the processor.
+//! If the slave PIC is responsible, it will ask the slave to send the interrupt number to the processor. The PIC that answers then looks for the "vector offset" and adds it to the input line to
+//! compute the interrupt number. The processor then acts on that interrupt number.
+//!
+//! ## The 8259 PIC today
+//! The 8259 PIC is now a legacy device that has been replaced by the APIC (Advanced Programmable Interrupt Controller). The APIC is usable within multiprocessor systems.
+//! The APIC can do more sophisticated and complex things than the 8259 PIC. Read more about the APIC
 //!
 //! ## Where can I read more?
 //! The following links are useful to learning more about the 8259 PIC and interrupts:
-//! - [8259 PIC on OSDev Wiki](https://wiki.osdev.org/PIC) (the page followed to write this module and where most of the documentation above is from)
+//! - [8259 PIC on OSDev Wiki](https://wiki.osdev.org/PIC)
 //! - [8259 PIC on Wikipedia](https://en.wikipedia.org/wiki/Intel_8259)
 //! - [Interrupts](https://wiki.osdev.org/IRQ)
 //!
@@ -83,6 +74,11 @@
 //!    }  
 //! }
 //! ```
+//!
+//! # Note
+//!
+//! Some bootloaders might mask all the IRQs from the 8259 (legacy) PIC, like Limine. Make sure you check the bootloader's documentation before
+//! you become confused due to this module not functioning as expected.
 
 use x86_64::instructions::port::Port;
 
